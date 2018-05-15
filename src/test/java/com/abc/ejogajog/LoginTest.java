d9b5909d58cffaf8171c40bc21a287e4e2000e57
hossainmtuhin <hmtuhin89@gmail.com>
2018-05-15 21:59:24 +0600

    Skeleton of project and LoginTest file is done.

---
+++ src/test/java/com/abc/ejogajog/LoginTest.java
@@ -0,0 +1,24 @@
+package com.abc.ejogajog;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void testLoginPageTitleCheck() {
+		
+	}
+	
+	@Test
+	public void testInvalidLogin() {
+		
+	}
+	
+	@Test
+	public void testValidLogin() {
+		
+	}
+	
+}
